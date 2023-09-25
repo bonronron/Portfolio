adae1980aabf1d375f37a07361587537ce93b8ff
bonronron <19hanro@gmail.com>
2023-09-25 23:49:15 +0100

    Adding code for other html elements and added other projects

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 $(document).ready(function(){
-    $.getJSON("projects.json", function(data){
-        $.each(data,function (index,value){
-            console.log(index,value);
-            $("#projects").append(`<li><a href="${value['link']}">
+    $.getJSON("projects.json", function(category){
+        // Games Project html
+        projects = category['games'];
+        $.each(projects,function(ix,value){
+            $(`#game-projects`).append(`<li><a class= "text-decoration-none" href="${value['link']}">
             <figure>
                 <img src= ${value['img']}>
                 <figcaption>
-                    <h3>${value['title']}</h3>
-                    <p>${value['description']}</p>
+                    <h6>${value['title']}</h6>
+                    <p><small>${value['description']}</small></p>
                 </figcaption>
             </figure> </a>
             </li>`);
-        })}).fail(function(){
+        });
+        // Literature Project html
+        projects = category['literature'];
+        $.each(projects,function(ix,value){
+            $(`#literature-projects`).append(`<li><a class= "text-decoration-none" href="${value['link']}">
+            <figure>
+                <img src= ${value['img']}>
+                <figcaption>
+                    <h6>${value['title']}</h6>
+                    <p><small>${value['description']}</small></p>
+                </figcaption>
+            </figure> </a>
+            </li>`);
+        });
+    }).fail(function(){
         console.log("An error has occurred.");
     });
 });
